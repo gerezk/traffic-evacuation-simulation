e0499468955fa84e17913ee699c91a115ecfd000
tortelliniEater <legit.im.user@gmail.com>
2026-01-14 16:58:20 +0100

    takes random edges and blocks both directions, tracks all adjacent streets and converts that into the rerouter.add.xml file

--- src/generate_uninformed_blockages.py
+++ src/generate_uninformed_blockages.py
@@ -1,10 +1,107 @@
+import traci
 import generate_cars as gc
 import xml.etree.ElementTree as ET
+from launcher import get_sumo_cmd
+from pathlib import Path
+import sumolib
+
+BLOCKED_ROADS_COUNT = 5
+TRIGGER_STEPS = 3
+OUT_FILE = "./data/rerouter.add.xml"
 
 taz_file = "./tmp/DangerTAZ.taz.xml"
 tree = ET.parse(taz_file)
 root = tree.getroot()
 
+net = sumolib.net.readNet("./data/neulengbach_sumo-webtools-osm.net.xml.gz")
 dangerRoads = gc.getEdgesFromTaz(root, "Danger_Zone_0")
 
-print(dangerRoads)
+def a(path):
+    return str((Path(__file__).parent / path).resolve())
+
+def get_adjacent_edges(start_edge, steps):
+    visited = set()
+    frontier = {start_edge}
+
+    for _ in range(steps):
+        next_frontier = set()
+
+        for edge in frontier:
+            if edge in visited:
+                continue
+
+            visited.add(edge)
+
+            for succ in net.getEdge(edge).getOutgoing():
+                next_frontier.add(succ.getID())
+
+            for pred in net.getEdge(edge).getIncoming():
+                next_frontier.add(pred.getID())
+
+        frontier = next_frontier
+
+    return visited
+
+args = [
+    "-n", a("../data/neulengbach_sumo-webtools-osm.net.xml.gz"),
+    "-a", a("../tmp/DangerTAZ.taz.xml"),
+]
+
+SUMO_CMD = get_sumo_cmd(args, gui=False)
+traci.start(SUMO_CMD)
+
+blocked_edges = set()
+trigger_edges = set()
+
+for _ in range(BLOCKED_ROADS_COUNT):
+    blocked = gc.getRandomEdge(dangerRoads)
+    blocked_edges.add(blocked)
+    print(f"added {blocked} to blocked_edges")
+    neighbors = get_adjacent_edges(blocked, TRIGGER_STEPS)
+    trigger_edges.update(neighbors)
+    
+    #Check for edge in other direction
+    if blocked.startswith("-"):
+        opposite = blocked[1:]
+    else:
+        opposite = "-" + blocked
+
+    if net.hasEdge(opposite):
+        blocked_edges.add(opposite)
+        print(f"added {opposite} to blocked_edges as opposite")
+        neighbors = get_adjacent_edges(blocked, TRIGGER_STEPS)
+        trigger_edges.update(neighbors)
+
+
+additional = ET.Element("additional")
+
+rerouter = ET.SubElement(
+    additional,
+    "rerouter",
+    {
+        "id": "rerouter",
+        "edges": " ".join(sorted(trigger_edges))
+    }
+)
+
+interval = ET.SubElement(
+    rerouter,
+    "interval",
+    {
+        "begin": "0",
+        "end": "1000000"
+    }
+)
+
+for edge in blocked_edges:
+    ET.SubElement(
+        interval,
+        "closingReroute",
+        {"id": edge} 
+    )
+
+
+tree = ET.ElementTree(additional)
+tree.write(OUT_FILE)
+
+traci.close()
